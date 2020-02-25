e0355bbc3c074185e9081a16cac868fa07e3802f
fatima <tima.alice806@gmail.com>
2020-02-25 17:03:05 -0600

    initial commit

---
+++ js/dice.js
@@ -0,0 +1,8 @@
+// const name = prompt("Please enter your name: ");
+// console.log(`my name is${name}`);
+
+function alertName(){
+    let myName = "Fatima";
+    alert(`Hi, my name is ${myName}`)
+}
+
