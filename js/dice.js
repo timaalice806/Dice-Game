5d1ce14cf19bbd3c394eb8af5b830788e955e876
fatima <tima.alice806@gmail.com>
2020-02-26 22:10:42 -0600

    added logic for players to roll dice and get sum, still working on removing the lowest 2 scores

--- js/dice.js
+++ js/dice.js
@@ -1,8 +1,31 @@
-// const name = prompt("Please enter your name: ");
-// console.log(`my name is${name}`);
+"use strict";
 
-function alertName(){
-    let myName = "Fatima";
-    alert(`Hi, my name is ${myName}`)
+let startGame = alert("Press button to start game");
+
+let players = [{score: 0}, {score: 0}, {score: 0}, {score: 0}, {score: 0}, {score: 0}, {score: 0}, {score: 0}, {score: 0}, {score: 0}];
+
+function rollDie (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    //document.getElementById("dr").innerHTML = ;
 }
+
+function rollDiePlayers () {
+    for(var i = 0; i < players.length; i++){
+        let dice4 = rollDie(1,4);
+        let dice6 = rollDie(1,6);
+        let dice8 = rollDie(1,8);
+        let dice10 = rollDie(1,10);
+        let dice12 = rollDie(1,12);
+        let dice20 = rollDie(1,20);
+     let sum = dice4 + dice6 + dice8 + dice10 + dice12 + dice20;
+     players[i].score = sum;
+    } 
+    console.log(players.score)
+}
+function removeLowestScore (players) {
+   Math.min.apply(null, players);
+}
+console.log(players.score);
+
+
 
